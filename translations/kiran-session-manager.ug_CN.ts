5c9a50f52459c5625e4c01dc394c05b2f9b9dfde
wangyucheng <wangyucheng@kylinsec.com.cn>
2023-04-03 10:33:03 +0800

    fix(translate):add some translation
    - 添加一些翻译
    
    Relates #67661

---
+++ translations/kiran-session-manager.ug_CN.ts
@@ -0,0 +1,76 @@
+<?xml version="1.0" ?><!DOCTYPE TS><TS version="2.1" language="zh_CN">
+<context>
+    <name>Kiran::SessionManager</name>
+    <message>
+        <location filename="../src/core/session-manager.cpp" line="295"/>
+        <source>This program is not responding</source>
+        <translation>بۇ پروگرامما ئىنكاس قايتۇرمايدۇ</translation>
+    </message>
+    <message>
+        <location filename="../src/core/session-manager.cpp" line="393"/>
+        <source>This program is blocking exit</source>
+        <translation>بۇ پروگرامما چېكىنىشنى توسىدۇ</translation>
+    </message>
+</context>
+<context>
+    <name>QObject</name>
+    <message>
+        <location filename="../lib/base/error.cpp" line="32"/>
+        <source>The phase cannot be register.</source>
+        <translation>باسقۇچنى تىزىملاتقىلى بولمايدۇ.</translation>
+    </message>
+    <message>
+        <location filename="../lib/base/error.cpp" line="34"/>
+        <source>The client %s already registered.</source>
+        <translation>خېرىدار ئاللىقاچان تىزىملاتقان.</translation>
+    </message>
+    <message>
+        <location filename="../lib/base/error.cpp" line="37"/>
+        <source>Failed to generate unique cookie.</source>
+        <translation>ئۆزگىچە پېچىنە ھاسىل قىلالمىدى.</translation>
+    </message>
+    <message>
+        <location filename="../lib/base/error.cpp" line="40"/>
+        <source>The inhibitor is not found.</source>
+        <translation>چەكلىگۈچ تېپىلمايدۇ.</translation>
+    </message>
+    <message>
+        <location filename="../lib/base/error.cpp" line="49"/>
+        <source>Internal error.</source>
+        <translation>ئىچكى خاتالىق.</translation>
+    </message>
+    <message>
+        <location filename="../lib/base/error.cpp" line="43"/>
+        <source>The status is invalid.</source>
+        <translation>ئورنى ئىناۋەتسىز.</translation>
+    </message>
+    <message>
+        <location filename="../lib/base/error.cpp" line="46"/>
+        <source>The action is not supported.</source>
+        <translation>بۇ ھەرىكەتنى قوللىمايدۇ.</translation>
+    </message>
+    <message>
+        <location filename="../lib/base/error.cpp" line="52"/>
+        <source>Unknown error.</source>
+        <translation>نامەلۇم خاتالىق.</translation>
+    </message>
+    <message>
+        <location filename="../lib/base/error.cpp" line="57"/>
+        <source> (error code: 0x%x)</source>
+        <translation>( خاتالىق كودى: 0x% x )</translation>
+    </message>
+</context>
+<context>
+    <name>main</name>
+    <message>
+        <location filename="../src/core/main.cpp" line="125"/>
+        <source>Specify a session type that contains required components.</source>
+        <translation>لازىملىق زاپچاسلارنى ئۆز ئىچىگە ئالغان يىغىن تۈرىنى بەلگىلەڭ.</translation>
+    </message>
+    <message>
+        <location filename="../src/core/main.cpp" line="126"/>
+        <source>session type</source>
+        <translation>يىغىن تىپى</translation>
+    </message>
+</context>
+</TS>
